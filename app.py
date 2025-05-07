e6ae4ec8d8ea358164b65bba9d1afc86279ae28f
Amna <amna@Amnas-MacBook-Air-2.local>
2025-05-06 21:05:39 -0400

    added code

--- app.py
+++ app.py
@@ -26,3 +26,52 @@ def file_preprocessing(file):
         final_texts += text.page_content
     
     return final_texts
+
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        'summarization',
+        model = base_model,
+        tokenizer = tokenizer,
+        max_length = 1000,
+        min_length = 50
+    )
+    input_text = file_preprocessing(filepath) 
+    result = pipe_sum(input_text) 
+    result = result[0]['summary_text'] 
+    return result
+
+@st.cache_data
+def displayPDF(file):
+    with open(file,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+# Embedding PDF in HTML
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+# Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+#streamlit code
+# Page configuration
+st.set_page_config(layout='wide', page_title="Summarization App")
+
+def main():
+    st.title('Document Summarization App using Language Model')
+    uploaded_file = st.file_uploader("Upload your PDF File", type=['pdf'])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/"+uploaded_file.name
+            with open(filepath, 'wb') as temp_file:
+                temp_file.write(uploaded_file.read())
+
+            with col1:
+                st.info("Uploaded PDF File")
+                pdf_viewer = displayPDF (filepath)
+
+            with col2:
+                st.info("Summarization is below")
+                summary = llm_pipeline(filepath)
+                st.success(summary) 
+
+if __name__ == '__main__':
+    main()
